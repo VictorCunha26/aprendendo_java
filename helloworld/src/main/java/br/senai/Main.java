b086a6397cadb47d82d3e1b01d05669eaae8d09a
VictorCunha26 <vicentinivictor130@gmail.com>
2026-08-05 21:46:12 -0300

    commit

---
+++ helloworld/src/main/java/br/senai/Main.java
@@ -0,0 +1,59 @@
+package br.senai;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /*System.out.println("Hello world!");
+        System.out.println("AIIIIII TULIAAOOOOOO");
+        System.out.println("QUEE ACERTUUUUUUUUU");
+
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.print("Olá " + nome);
+        System.out.println(" você tem " + idade + " anos");*/
+
+        /*int idade = 40;
+        double salario = 1000.00;
+        char sexo = 'M';
+        boolean ativo = true;
+        String nome = "Vitinho sobe capa";
+
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Salário: " + salario);
+        System.out.println("Sexo: " + sexo);
+        System.out.println("Ativo: " + ativo);*/
+
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um número: ");
+
+        int num = scanner.nextInt();
+
+        System.out.println("Anterior: " + (num-1));
+        System.out.println("Atual: " + (num));
+        System.out.println("Sucessor: " + (num + 1));*/
+
+        //Exercício 5
+
+        Scanner scanner = new Scanner (System.in);
+
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+        System.out.println("1° número: " + num1);
+        System.out.println("2° número: " + num2);
+        Sys
+
+
+        
+
+    }
+}
